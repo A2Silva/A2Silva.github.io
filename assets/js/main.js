6209291be4ef78d53120c285b2e8b0b725831fa6
A2Silva <andrew03silva@yahoo.com>
2025-07-08 10:39:33 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  setTimeout(() => {
+    preloader.style.opacity = "0";
+    preloader.style.transition = "opacity 1s ease";
+    setTimeout(() => preloader.remove(), 1000);
+  }, 3500); // 3s draw + 0.5s buffer
+});
 
 	
 		
