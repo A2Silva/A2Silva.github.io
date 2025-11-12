ad762f25a1fe62f7aaeba18e5a9e15d2f5182a06
A2Silva <andrew03silva@yahoo.com>
2025-11-12 11:27:30 -0600

    Add files via upload

--- main.js
+++
@@ -1,8 +0,0 @@
-const images = ['bg01.jpg', 'bg02.jpg', 'bg03.jpg'];
-let index = 0;
-
-setInterval(() => {
-  index = (index + 1) % images.length;
-  document.querySelector('.background-layer').style.backgroundImage = `url('assets/images/${images[index]}')`;
-}, 6000); // every 6 seconds
-
